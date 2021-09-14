2450a2620f2390c913ba435c57ea7b12376de5a1
SirMarshall <carterwalker@knights.ucf.edu>
2021-09-13 22:01:30 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Carter I. Walker
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Necessary Variables
+        Scanner input = new Scanner(System.in);
+        double height, weight;
+        double bmi;
+
+        //Query for height and weight
+        System.out.println("Please enter your height in inches:");
+        height = input.nextInt();
+
+        System.out.println("Please enter your weight in pounds:");
+        weight = input.nextInt();
+
+        //Calculation
+        bmi = (weight / (height * height)) * 703;
+
+        //Output
+        if (bmi < 18.5)
+        {
+            System.out.format("According to our calculations, your BMI (%.1f) registers you as underweight, we recommend you consult your doctor.", bmi);
+        }
+        else if (bmi > 25)
+        {
+            System.out.format("According to our calculations, your BMI (%.1f) registers you as overweight, we recommend you consult your doctor.", bmi);
+        }
+        else
+        {
+            System.out.format("According to our calculations, your BMI (%.1f) registers you in the ideal weight range!", bmi);
+        }
+    }
+}
